5c4b4938ce7e5663576058cded8625164c505466
3alifaisal <alifaisalawada000@gmail.com>
2024-12-31 16:15:36 +0100

    done everything but interface
    finished all the models, with relationships and demo seeding.

--- app/models.py
+++ app/models.py
@@ -22,6 +22,8 @@ class Employee(db.Model, UserMixin):  # Multiple inheritance
         db.String(255), nullable=False
     )  # Hashed password for authentication
 
+    orders = db.relationship("Order", back_populates="employee")
+
     @property
     def password(self):
         return self.hashed_password
@@ -32,3 +34,76 @@ class Employee(db.Model, UserMixin):  # Multiple inheritance
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+
+class Menu(db.Model):
+    __tablename__ = "menus"
+
+    # Primary key
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(30), nullable=False)  # Name of the item
+    items = db.relationship("MenuItem", back_populates="menu")
+
+
+class MenuItem(db.Model):
+    __tablename__ = "menu_items"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    menu_id = db.Column(db.Integer, db.ForeignKey("menus.id"), nullable=False)
+    menu_type_id = db.Column(
+        db.Integer, db.ForeignKey("menu_item_types.id"), nullable=False
+    )
+
+    # Relationships
+    menu = db.relationship("Menu", back_populates="items")
+    type = db.relationship("MenuItemType", back_populates="items")
+    order_details = db.relationship("OrderDetail", back_populates="menu_item")
+
+
+class MenuItemType(db.Model):
+    __tablename__ = "menu_item_types"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+
+    items = db.relationship("MenuItem", back_populates="type")
+
+
+class Table(db.Model):
+    __tablename__ = "tables"
+
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer, nullable=False, unique=True)
+    capacity = db.Column(
+        db.Integer,
+        nullable=False,
+    )
+    orders = db.relationship("Order", back_populates="table")
+
+
+class Order(db.Model):
+    __tablename__ = "orders"
+
+    id = db.Column(db.Integer, primary_key=True)
+    employee_id = db.Column(db.Integer, db.ForeignKey("employees.id"), nullable=False)
+    table_id = db.Column(db.Integer, db.ForeignKey("tables"), nullable=False)
+    finished = db.Column(db.boolean, nullable=False)
+
+    employeee = db.relationship("Employee", back_populates="orders")
+    table = db.relationship("Table", back_populates="orders")
+    details = db.relationship(
+        "OrderDetail", back_populates="order", cascade="all, delete-orphan"
+    )
+
+
+class OrderDetail(db.Model):
+    __tablename__ = "order_details"
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey("orders.id"), nullable=False)
+    menu_item_id = db.Column(db.Integer, db.ForeignKey("menu_items.id"), nullable=False)
+
+    order = db.relationship("Order", back_populates="details")
+    menu_item = db.relationship("MenuItem", back_populates="order_details")
--- database.py
+++ database.py
@@ -7,8 +7,8 @@ load_dotenv()
 # so that the environment variables are
 # properly loaded.
 from app import app, db
-from app.models import Employee
-
+from app.models import Employee, Menu, MenuItem, MenuItemType, Table
+import random
 
 with app.app_context():
     db.drop_all()
@@ -17,3 +17,24 @@ with app.app_context():
     employee = Employee(name="Margot", employee_number=1234, password="password")
     db.session.add(employee)
     db.session.commit()
+
+    beverages = MenuItemType(name="Beverages")
+    entrees = MenuItemType(name="Entrees")
+    sides = MenuItemType(name="Sides")
+
+    dinner = Menu(name="Dinner")
+
+    fries = MenuItem(name="French fries", price=3.50, type=sides, menu=dinner)
+    drp = MenuItem(name="Dr. Pepper", price=1.0, type=beverages, menu=dinner)
+    jambalaya = MenuItem(name="Jambalaya", price=21.98, type=entrees, menu=dinner)
+
+    # Add instances to the session
+    db.session.add(dinner)
+
+    # Commit the changes to the database
+    db.session.commit()
+
+    tables = [Table(number=i, capacity=random.randint(2, 10)) for i in range(0, 10)]
+    db.session.add_all(tables)
+    db.session.commit()
+    print("successfully added")
